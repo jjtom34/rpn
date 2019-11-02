58e0e2bfa878c68d1d1e9b6c09f0a9242c382b38
Jonathan Tom <jjtom@umich.edu>
2019-11-02 12:12:23 -0400

    Testing coverage

--- rpn.py
+++ rpn.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import operator
-
+import readline
 
 operators = {
     '+': operator.add,
@@ -10,6 +10,9 @@ operators = {
     '/': operator.truediv,
     '^': operator.pow,
 }
+
+def foo(bar):
+    return
 
 
 def calculate(myarg):
